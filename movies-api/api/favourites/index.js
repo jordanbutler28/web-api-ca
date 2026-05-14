255b73adffd1502530895f2f185d0ba036f68db6
Jordan Butler <20106583@mail.wit.ie>
2026-05-14 15:56:30 +0100

    Added post function to add movie to favourites

--- movies-api/api/favourites/index.js
+++ movies-api/api/favourites/index.js
@@ -1,13 +1,23 @@
 import express from 'express';
-//import asyncHandler from 'express-async-handler';
+import asyncHandler from 'express-async-handler';
 import Favourite from './favouriteModel'
 
 const router = express.Router();
 
-// Get favourites
+//Get favourites
 router.get('/', async (req, res) => {
     const favourites = await Favourite.find();
     res.status(200).json(favourites);
 });
+
+//post new favourites
+router.post('/', asyncHandler(async (req, res) => {
+  //get movie id and title as favourite model requires
+  const { movieId, title } = req.body;
+
+  //add favourites to mongo
+  await Favourite.create({ movieId, title });
+  res.status(201).json({ success: true, msg: 'Favourite successfully added' });
+}));
 
 export default router;
